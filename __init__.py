0381429fe571128de31c57b84826209fc5f5f5ba
Jochen Topf <jochen@topf.org>
2014-05-06 11:30:22 +0200

    Consolidate QGIS2 changes, update metadata

--- __init__.py
+++ __init__.py
@@ -1,14 +1,4 @@
 #!python
-def name():
- return "OSMEditorRemoteControl"
-def description():
- return "OSM Editor remote control."
-def version():
- return "Version 1.0"
-def qgisMinimumVersion(): 
- return "1.5"
-def authorName():
- return "Jochen Topf"
 def classFactory(iface):
  from plugin import OSMEditorRemoteControlPlugin
  return OSMEditorRemoteControlPlugin(iface)
